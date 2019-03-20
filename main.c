b9e288d607b796c418d3235ffaa12537865c52e0
will199608 <wiwiyu.will@gmail.com>
2019-03-20 09:12:23 +0800

    homework3

--- main.c
+++ main.c
@@ -1,5 +1,26 @@
+int sub(int a, int b)
+{
+    return (a-b);
+}
+
+
+// pass multiple paramter
+int add(int a, int b, int c, int d, int e, int f)
+{
+        return a + b + c + d + e + f;
+}
+
 void reset_handler(void)
 {
-	while (1)
-		;
+        int a = 7, b = 5;
+        int c;
+
+                // pass two integers
+        c = sub(a, b);
+
+                    // pass six integers
+        c = add(1, 2, 3, a, b, c);
+
+        while (1)
+         ;
 }
